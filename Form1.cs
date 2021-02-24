45317d90762142d36f6f5fddede610268f9c41ca
Mihir Ravishankar <mihir.ravishankar@gmail.com>
2021-02-24 13:08:29 +0800

    Async Await C# Code

---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace Async_Await_Program
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int CharacterCounter()
+        {
+            int numChar = 0;
+            using (StreamReader reader = new StreamReader(@"C:\Users\Mihir\Desktop\Robonomics Internship\Email.txt"))
+            {
+                string extract = reader.ReadToEnd();
+                numChar = extract.Length;
+                Thread.Sleep(5000);
+            }
+
+            return numChar;
+        }
+
+        private async void Button_PF_Click(object sender, EventArgs e)
+        {
+            Task<int> task = new Task<int>(CharacterCounter);
+            task.Start();
+
+            Counter.Text = "Processing file. Please wait ...";
+            int count = await task;
+            Counter.Text = count.ToString() + " characters in file.";
+        }
+    }
+}
